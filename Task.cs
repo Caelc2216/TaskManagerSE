2ed75dc203f41dd813494a61e613994d1647a142
SaffronGamer <saffrongamer167@gmail.com>
2025-02-07 10:33:01 -0700

    Implemented Save, Load, and Task class

---
+++ Task.cs
@@ -0,0 +1,11 @@
+namespace TaskManager {
+    public class Task(string Name, string Description, DateTime Timeline, int Id) {
+        public string Name { get; set; } = Name;
+        public string Description { get; set; } = Description;
+        public DateTime Timeline { get; set; } = Timeline;
+        public int Id { get; set; } = Id;
+        public bool IsComplete { get; set; } = false;
+        public void MarkAsComplete() => IsComplete = !IsComplete;
+        public override string ToString() =>  $"Task: {Name}, Description: {Description}, Timeline: {Timeline}, Id: {Id}";
+    }
+}
